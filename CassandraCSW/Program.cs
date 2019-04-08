03d2b67e8ebcc8df4a1df04d25807d94c3e8a476
unknown <user-admin@PT-PF1F3RYY.critical.pt>
2019-04-08 17:41:46 +0100

    refac

--- CassandraCSW/Program.cs
+++ CassandraCSW/Program.cs
@@ -1,29 +1,23 @@
 ﻿using Cassandra;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CassandraCSW
 {
     class Program
     {
-        private static ISession _session { get; set; }
+        public static Repository repository { get; set; }
         static void Main(string[] args)
         {
+            repository = new Repository();
             var showOptions = true;
-
-            var cluster = Cluster.Builder()
-                     .AddContactPoints("127.0.0.1")
-                     .Build();
-            _session = cluster.Connect("cswkeyspace");
+            
             while (showOptions)
             {
                 ShowOptions();
                 var option = Console.ReadLine();
 
-                switch (option)
+                switch (option.ToUpper())
                 {
                     case "C":
                         CreateOrderStatus();
@@ -37,37 +31,46 @@ namespace CassandraCSW
                     case "D":
                         DeleteOrderStatus();
                         break;
+                    case "S":
+                        showOptions = false;
+                        break;
                     default:
+                        showOptions = false;
                         break;
                 }
-
-                Console.WriteLine("Deseja continuar?(Y/N)");
-                if (Console.ReadLine() != "Y")
-                    showOptions = false;
             }
-
-
         }
+
         public static void ShowOptions()
         {
+            Console.WriteLine();
+            Console.WriteLine("--------------------------");
             Console.WriteLine("Digite o comando desejado:");
+            Console.WriteLine();
             Console.WriteLine("C = Criar um novo status");
             Console.WriteLine("R = Listar status");
             Console.WriteLine("U = Alterar um status");
             Console.WriteLine("D = Remover um status");
+            Console.WriteLine("S = Sair");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine();
+            
         }
 
         public static void CreateOrderStatus()
         {
             Console.WriteLine("Digite o nome do novo status:");
             var name = Console.ReadLine();
-            var rs = _session.Execute($"INSERT INTO cswkeyspace.order_status(id, status) VALUES({Guid.NewGuid()}, '{name}')");
-            Console.WriteLine($"Response: {rs}");
+            var result = repository.CreateOrderStatus(name);
+            if (result)
+                Console.WriteLine("Criado com sucesso");
+            else
+                Console.WriteLine("Não foi possível criar o novo status");
         }
 
         public static void ListOrderStatus()
         {
-            var rs = _session.Execute("SELECT * FROM order_status");
+            var rs = repository.ListOrderStatus();
             foreach (var row in rs)
             {
                 Console.WriteLine($"Id:{row.GetValue<Guid>("id")}| Name:{row.GetValue<string>("status")}");
@@ -77,37 +80,27 @@ namespace CassandraCSW
         public static void UpdateOrderStatus()
         {
             Console.WriteLine("Digite o nome do status que deseja alterar:");
-            var status = Console.ReadLine();
-            var selectCommand = _session.Execute($"SELECT * FROM order_status WHERE status = '{status}' ALLOW FILTERING");
-            var result = selectCommand.GetRows().First().GetValue<Guid>("id");
-            if (result!=null)
-            {
-                Console.WriteLine("Digite o novo nome que deseja alterar:");
-                var newName = Console.ReadLine();
-    
-                var updateCommand = _session.Execute($"UPDATE cswkeyspace.order_status SET status = '{newName}' WHERE id = {result}");
-                Console.WriteLine($"Response: {updateCommand}");
+            var oldStatus = Console.ReadLine();
+            Console.WriteLine("Digite o novo nome que deseja alterar:");
+            var newStatus = Console.ReadLine();
 
-            }
+            var result = repository.UpdateOrderStatus(oldStatus, newStatus);
+            if (result)
+                Console.WriteLine("Atualizado com sucesso");
             else
-                Console.WriteLine("Não foi possível localizar o status");
-
+                Console.WriteLine("Não foi possível atualizar o status");
         }
 
         public static void DeleteOrderStatus()
         {
             Console.WriteLine("Digite o nome do status que deseja remover:");
             var status = Console.ReadLine();
-            var selectCommand = _session.Execute($"SELECT * FROM order_status WHERE status = '{status}' ALLOW FILTERING");
-            if (selectCommand.SingleOrDefault() != null)
-            {
-                var id = selectCommand.SingleOrDefault().GetValue<Guid>("id");
-                var deleteCommand = _session.Execute($"DELETE FROM cswkeyspace.order_status WHERE id = {id}");
-                Console.WriteLine($"Response: {deleteCommand}");
-            }
-            else
-                Console.WriteLine("Não foi possível localizar o status");
 
+            var result = repository.DeleteOrderStatus(status);
+            if (result)
+                Console.WriteLine("Removido com sucesso");
+            else
+                Console.WriteLine("Não foi possível remover o status");
         }
     }
 }
---
+++ CassandraCSW/Repository.cs
@@ -0,0 +1,89 @@
+﻿using Cassandra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CassandraCSW
+{
+    public class Repository
+    {
+        private static ISession _session { get; set; }
+
+        public Repository()
+        {
+            var cluster = Cluster.Builder()
+                     .AddContactPoints("127.0.0.1")
+                     .Build();
+            _session = cluster.Connect("cswkeyspace");
+        }
+
+        public bool CreateOrderStatus(string name)
+        {
+            try
+            {
+                var result = _session.Execute($"INSERT INTO cswkeyspace.order_status(id, status) VALUES({Guid.NewGuid()}, '{name}')");
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+          
+            return true;
+        }
+
+        public RowSet ListOrderStatus()
+        {
+            return _session.Execute("SELECT * FROM order_status");
+        }
+
+        private RowSet GetOrderStatusByName(string name)
+        {
+            var selectCommand = _session.Execute($"SELECT * FROM order_status WHERE status = '{name}' ALLOW FILTERING");
+            return selectCommand;
+        }
+
+        public bool UpdateOrderStatus(string oldName, string newName)
+        {
+            try
+            {
+                var oldOrder = GetOrderStatusByName(oldName);
+                var result = oldOrder.GetRows().FirstOrDefault();
+                if (result != null)
+                {
+                    var updateCommand = _session.Execute($"UPDATE cswkeyspace.order_status SET status = '{newName}' WHERE id = {result.GetValue<Guid>("id")}");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+            }
+            return false;
+
+        }
+
+        public bool DeleteOrderStatus(string status)
+        {
+            try
+            {
+                var oldOrder = GetOrderStatusByName(status);
+                var result = oldOrder.GetRows().FirstOrDefault();
+                if (result != null)
+                {
+                    var deleteCommand = _session.Execute($"DELETE FROM cswkeyspace.order_status WHERE id = {result.GetValue<Guid>("id")}");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return false;
+
+        }
+
+    }
+}
